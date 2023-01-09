7c8e29a182adc250c198acb45bcd4bc44e0a0551
hashimoto-masataka <115071516+hashimoto-masataka@users.noreply.github.com>
2023-01-09 12:05:12 +0000

    deviseインストール,user/adminモデル作成済

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  scope module: :public do
+  root to: "homes#top"
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
