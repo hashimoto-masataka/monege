d5e615f6ff149373aa0f2fbdb7017e4717cdd003
hashimoto-masataka <115071516+hashimoto-masataka@users.noreply.github.com>
2023-01-11 02:39:55 +0000

    月毎に表示させる

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
- scope module: :public do
+ devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
+ scope module: :public do
+  root to: "homes#top"
   resources :users, only: [:index, :update,]
    get "/users/my_page" => "users#show"
    get "/users/information/edit" => "users#edit"
@@ -16,18 +24,10 @@ Rails.application.routes.draw do
 
 end
 
-  devise_for :admin,skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
+ namespace :admin do
 
-  scope module: :public do
   root to: "homes#top"
-  end
-
+ end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
