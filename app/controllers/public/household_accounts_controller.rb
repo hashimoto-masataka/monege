bcf9b2f2499e59e8543415ee436542ed77dd7510
hashimoto-masataka <115071516+hashimoto-masataka@users.noreply.github.com>
2023-01-12 12:48:39 +0000

    失敗

--- app/controllers/public/household_accounts_controller.rb
+++ app/controllers/public/household_accounts_controller.rb
@@ -9,35 +9,19 @@ class Public::HouseholdAccountsController < ApplicationController
    end
 
   def create
-    # レコード生成に失敗した場合ロールバックする
-    # ref: https://blog.furu07yu.com/entry/rails-transaction
-    ActiveRecord::Base.transaction do
-      household = Household.create(user_id: current_user.id)
-      current_user.categories.each do |category|
-        HouseholdAccount.create(
-            household_id: household.id,
-            category_name: category.category_name,
-            color: category.color,
-            target_price: category.target_price
-          )
-      end
-    end
-    redirect_to root_path, notice: "You have created household_account successfully."
-    # @household_account = HouseholdAccount.new(household_account_params)
-    # @household_account.customer_id = current_customer.id
-    # if @household_account.save
-    #   redirect_to household_account_path(@household_account), notice: "You have created household_account successfully."
-    # else
-    #   @household_accounts=HouseholdAccount.all
-
-    #   render 'show'
-    # end
-  rescue
+    @household_account = Household.new(household_account_params)
     @user = current_user
+    @household_account.user_id= @user.id
     @categories = current_user.categories
-    @household_accounts=HouseholdAccount.all
-    render 'show'
+    if @household_account.save
+      redirect_to root_path
+      flash.now[:notice] = "家計簿を投稿しました"
+    else
+      render :index
+    end
   end
+
+
 
   def index
     @categories = current_user.categories
@@ -64,7 +48,7 @@ class Public::HouseholdAccountsController < ApplicationController
   private
 
   def household_account_params
-    params.require(:household_account).permit(:category_id)
+    params.require(:household_account).permit(:category_name, :target_price)
   end
 
   def ensure_guest_user
